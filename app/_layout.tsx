310cc1a62f16f13e5cde5d971af402b4acdcf467
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2026-01-30 00:24:02 +0530

    layout file fixed with colour styling for theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { AppThemeProvider } from '@/hooks/useTheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import "./global.css"// just needed to be imported here to work with nativewind
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import './global.css'; // just needed to be imported here to work with nativewind
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,12 +14,14 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <AppThemeProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </AppThemeProvider>
   );
 }
