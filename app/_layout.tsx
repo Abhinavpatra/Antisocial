1350e74b488241509639e818c72ae3fc00ebf853
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2026-01-29 17:57:15 +0530

    Tailwind setup and imports completed in the app folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import "./global.css"// just needed to be imported here to work with nativewind
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
